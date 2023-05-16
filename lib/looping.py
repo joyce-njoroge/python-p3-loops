8e86ca677e2579837361cf47f17c034204f30251
Joyce Njoroge <joycewambuku33@gmail.com>
2023-05-16 10:11:30 +0300

    complete

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,34 @@
 
 def happy_new_year():
     # code goes here!
+    countdown = ""
+    number = 10
+
+    while number > 1:
+        countdown += str(number) + "\n"
+        number -= 1
+
+    countdown += "1\n"
+    countdown += "Happy New Year!"
+
+    assert countdown == '10\n9\n8\n7\n6\n5\n4\n3\n2\n1\nHappy New Year!', "Unexpected output"
+    print(countdown)
     pass
 
 def square_integers(int_list):
     # code goes here!
+    return [x ** 2 for x in int_list]
     pass
 
 def fizzbuzz():
     # code goes here!
+    for num in range(1, 101):
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")
+        else:
+            print(num)
     pass
